e31c29f022f0703cf14f15531ec0077135fa4056
Mnurg <nurgunawan@gmail.com>
2021-03-26 18:58:10 +0700

    Demo

--- hello/views.py
+++ hello/views.py
@@ -4,15 +4,11 @@ from django.http import HttpResponse
 from .models import Greeting
 import requests
 # Create your views here.
-# def index(request):
-    # return HttpResponse('Hello from Python!')
-    #return render(request, "index.html")
-
-# Modifikasi Index
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+
+
 
 def db(request):
 
